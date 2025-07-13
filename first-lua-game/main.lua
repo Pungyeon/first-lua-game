cfc09ba3e3d8b69aed87621a352f9f743585ff06
Mobile User <mobile@localhost>
2025-07-13 16:02:57 +0200

    save local changes at 2025-07-13 16:02:57

--- first-lua-game/main.lua
+++ first-lua-game/main.lua
@@ -1,21 +1,9 @@
 local color = require('color')
 local Node = require('node')
 local Player = require('player')
+local Puck = require('puck')
 local InputComponent = require('input_component')
 local globals = require('globals')
-
-
--- Puck class inherits from Node but has no input
-Puck = {}
-Puck.__index = Puck
-Puck = setmetatable(Puck, { __index = Node })
-
-function Puck:new(x, y)
-    local obj = Node.new(self, x, y, 20, 20)
-		setmetatable(obj, self)
-		return obj
-end
-
 
 function checkCollision(a, b)
     return a.x < b.x + b.width and
@@ -24,7 +12,6 @@ function checkCollision(a, b)
            a.y + a.height > b.y
 end
 
--- Love2D callbacks
 function love.load()
     player = Player:new(100, 100, InputComponent:new(globals.InputMap))
     puck = Puck:new(300, 300)
@@ -35,9 +22,6 @@ function love.update(dt)
     puck:update(dt)
 
 		if checkCollision(player, puck) then
-			-- TODO : @pungyeon - we should have the player push the puck somehow
-			--  Or simply just pick up and carry the puck like a normal ice-hockey
-			--  game.  
 			puck.color = color.GREEN
 		else 
 			puck.color = color.WHITE
---
+++ first-lua-game/puck.lua
@@ -0,0 +1,14 @@
+
+-- Puck class inherits from Node but has no input
+Puck = {}
+Puck.__index = Puck
+Puck = setmetatable(Puck, { __index = Node })
+
+function Puck:new(x, y)
+    local obj = Node.new(self, x, y, 20, 20)
+		obj.
+		setmetatable(obj, self)
+		return obj
+end
+
+return Puck
