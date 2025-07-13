6d0675b472affe64935b204573d36f4261860812
Mobile User <mobile@localhost>
2025-07-13 09:54:24 +0200

    save local changes at 2025-07-13 09:54:24

--- first-lua-game/main.lua
+++ first-lua-game/main.lua
@@ -1,13 +1,18 @@
 local lg = love.graphics
 local screenWidth, screenHeight = love.window.getMode()
 
+local player_x = 50
+local player_y = 50
+local player_width = 30
+local player_height = 30
+
 function love.load()
 
 end
 
 function love.update(dt)
-	-- This is where the movement and environment logic should go
 	if love.keyboard.isDown('w') then
+		player_y = player_y - 1
 	end 
 
 end
@@ -15,7 +20,7 @@ end
 
 function love.draw()
   -- This is where we draw all the elements that require drawing
-	lg.rectangle("fill", 50, 50, 30, 30)
+	lg.rectangle("fill", player_x, player_y, player_width, player_height)
 end
 
 function love.resize(w, h)
