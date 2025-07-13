c9b9fa6269ffc85210dbaedb3298bbd2fd609ac6
Mobile User <mobile@localhost>
2025-07-13 21:45:28 +0200

    save local changes at 2025-07-13 21:45:28

--- first-lua-game/goal.lua
+++ first-lua-game/goal.lua
@@ -10,9 +10,16 @@ function Goal:new(side, x, y, width, height)
     local obj = Node.new(self, x, y, width, height)
 		obj.color = color.DARK_GREEN
 		obj.can_score = true
+
+		obj.top_post = Node.new(self, x, y, width, height*0.1)
+		obj.bottom_post = Node.new(self, )
 		obj.side = side
 		setmetatable(obj, self)
     return obj
+end
+
+function Goal:draw()
+	
 end
 
 function Goal:collision(puck) 
@@ -27,36 +34,7 @@ function Goal:collision(puck)
 end
 
 function Goal:bounce_puck(puck)
-	-- Puck touches bottom of goal from outside
-	if puck.y < self.y + self.height and puck.y + (puck.height/2) > self.y + self.height then
-		puck.vy = puck.vy * -1
-		return false
-	end
 	
-	-- Puck touches back of goal from outside
-	if puck.x > self.x and puck.x + (puck.width / 2) < self.x then 
-		puck.vx = puck.vx * -1
-		return false
-	end
-
-	-- Puck touch top of goal from outside
-	  if puck.
-
-
-	if puck.x < self.x then
-		puck.speed = puck.speed * 0.2
-		puck.vx = puck.vx * -1
-	end
-	
-	if puck.y < self.y then
-		puck.speed = puck.speed * 0.2
-		puck.vy = puck.vy * -1
-	end
-
-	if puck.y > self.y + self.height then
-		puck.speed = puck.speed * 0.2
-		puck.vy = puck.vy * -1
-	end
 end
 
 return Goal
