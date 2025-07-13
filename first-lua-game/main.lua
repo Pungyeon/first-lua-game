3e145b80868d463cc3dd64b0f2630d2cbe5363ca
Mobile User <mobile@localhost>
2025-07-13 10:01:39 +0200

    save local changes at 2025-07-13 10:01:39

--- first-lua-game/main.lua
+++ first-lua-game/main.lua
@@ -1,33 +1,60 @@
-local lg = love.graphics
-local screenWidth, screenHeight = love.window.getMode()
+Player = {}
+Player.__index = Player
 
-local player_x = 50
-local player_y = 50
-local player_width = 30
-local player_height = 30
+function Player:new(x, y)
+    local obj = {
+        x = x,
+        y = y,
+        speed = 200,
+        vx = 0,
+        vy = 0,
+        size = 50
+    }
+    setmetatable(obj, self)
+    return obj
+end
 
+function Player:update(dt)
+    self.vx, self.vy = 0, 0
+
+    for key, action in pairs(InputMap) do
+        if love.keyboard.isDown(key) then
+            action(self)
+        end
+    end
+
+    -- Normalize diagonal movement
+    local mag = math.sqrt(self.vx^2 + self.vy^2)
+    if mag > 0 then
+        self.vx = self.vx / mag
+        self.vy = self.vy / mag
+    end
+
+    self.x = self.x + self.vx * self.speed * dt
+    self.y = self.y + self.vy * self.speed * dt
+end
+
+function Player:draw()
+    love.graphics.rectangle("fill", self.x, self.y, self.size, self.size)
+end
+
+-- Input mapping
+InputMap = {
+    w = function(p) p.vy = p.vy - 1 end,
+    s = function(p) p.vy = p.vy + 1 end,
+    a = function(p) p.vx = p.vx - 1 end,
+    d = function(p) p.vx = p.vx + 1 end
+}
+
+-- Love2D callbacks
 function love.load()
-
+    player = Player:new(100, 100)
 end
 
 function love.update(dt)
-	if love.keyboard.isDown('w') then
-		player_y = player_y - 1
-	end 
-
+    player:update(dt)
 end
-
 
 function love.draw()
-  -- This is where we draw all the elements that require drawing
-	lg.rectangle("fill", player_x, player_y, player_width, player_height)
+    player:draw()
 end
-
-function love.resize(w, h)
-   print("love.resize")
-   lg.clear()
-
-   lg.present()
-end
-
-
