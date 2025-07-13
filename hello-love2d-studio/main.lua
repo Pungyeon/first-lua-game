23f648c60251434f613aaad81960f2cf4712628f
Mobile User <mobile@localhost>
2025-07-13 09:39:02 +0200

    save local changes at 2025-07-13 09:39:02

--- hello-love2d-studio/main.lua
+++ hello-love2d-studio/main.lua
@@ -1,33 +1,12 @@
-local textToPrint  = 'love2d studio\nCode and debug Love2D games \nAll on your mobile device'
-local printedText  = ""
-local bg = require('background')
-local interval = 0.2
-local typeTimer = interval
-local typePosition = 0
-local waitCycles = 20
 local lg = love.graphics
 local screenWidth, screenHeight = love.window.getMode()
 
 function love.load()
-  local fontSize = 32
-  if screenHeight < 500 or screenWidth < 500 then
-     fontSize = 16
-  end
-  local textFont = lg.newFont(fontSize)
-  lg.setFont(textFont)
+
 end
 
 function love.update(dt)
-    typeTimer = typeTimer - dt
-    if typeTimer <= 0 then
-        typeTimer = interval
-        typePosition = typePosition + 1
-      if typePosition >#textToPrint + waitCycles then
-          typePosition = 0
-      end
-
-        printedText = string.sub(textToPrint,0,typePosition)
-    end
+	-- This is where the movement and environment logic should go
 end
 
 
