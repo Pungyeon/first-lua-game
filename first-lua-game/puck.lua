b16f7393a6bf4a167ca17fd1fca77b5f6650f51c
Mobile User <mobile@localhost>
2025-07-13 16:07:54 +0200

    save local changes at 2025-07-13 16:07:54

--- first-lua-game/puck.lua
+++ first-lua-game/puck.lua
@@ -6,9 +6,18 @@ Puck = setmetatable(Puck, { __index = Node })
 
 function Puck:new(x, y)
     local obj = Node.new(self, x, y, 20, 20)
-		obj.
+		obj.vx = 1
+		obj.vy = 2
 		setmetatable(obj, self)
 		return obj
+end
+
+function Puck:update(dt)
+		if self.vx > 0 then 
+			self.vx 
+		end 
+
+		Node:update(self, dt)
 end
 
 return Puck
