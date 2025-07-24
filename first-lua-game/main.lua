2811bcaf082bd7b5e02b530bced83d510850b95e
Lasse Martin Jakobsen <lasse@tengen.dk>
2025-07-24 13:36:05 +0000

    fixed external collision issue

--- first-lua-game/main.lua
+++ first-lua-game/main.lua
@@ -15,140 +15,139 @@ local selected = 1
 
 local goal_height = 140
 local goal_width = 40
-local goal_center = (screenHeight / 2) - (goal_height/2)
+local goal_center = (screenHeight / 2) - (goal_height / 2)
 
 
 function checkWallCollision(a)
-	return a.x < 0 or a.y < 0 or a.x > screenWidth or a.y > screenHeight
+    return a.x < 0 or a.y < 0 or a.x > screenWidth or a.y > screenHeight
 end
 
 function love.load()
-    puck = Puck:new(screenWidth/2, screenHeight/2)
+    puck = Puck:new(screenWidth / 2, screenHeight / 2)
 
-		home_team = Players:new({
-			Player:new(
-				screenWidth/2 + 100,
-				screenHeight/2 + 100,
-				color.RED,
-				InputComponent:new(globals.InputMap)
-			),
-			Player:new(
-				screenWidth/2 + 100,
-				screenHeight/2 - 100,
-				color.RED,
-				InputComponent:new(globals.InputMap)
-			)
-		})
+    home_team = Players:new({
+        Player:new(
+            screenWidth / 2 + 100,
+            screenHeight / 2 + 100,
+            color.RED,
+            InputComponent:new(globals.InputMap)
+        ),
+        Player:new(
+            screenWidth / 2 + 100,
+            screenHeight / 2 - 100,
+            color.RED,
+            InputComponent:new(globals.InputMap)
+        )
+    })
 
-		away_team = Players:new({
-			Player:new(
-				screenWidth/2 - 100,
-				screenHeight/2 + 100,
-				color.GREEN,
-				nil
-			),
-			Player:new(
-				screenWidth/2 - 100,
-				screenHeight/2 - 100,
-				color.GREEN,
-				nil
-			)
-		})
+    away_team = Players:new({
+        Player:new(
+            screenWidth / 2 - 100,
+            screenHeight / 2 + 100,
+            color.GREEN,
+            nil
+        ),
+        Player:new(
+            screenWidth / 2 - 100,
+            screenHeight / 2 - 100,
+            color.GREEN,
+            nil
+        )
+    })
 
-		goalie = Goalie:new(210, 380, color.GREEN, {})
-		
-		goal = Goal:new(-1, 150, goal_center, goal_width, goal_height)
+    goalie = Goalie:new(210, 380, color.GREEN, {})
+
+    goal = Goal:new(-1, 150, goal_center, goal_width, goal_height)
 end
 
 function love.keypressed(key)
-		if key == 'k' then
-			home_team:switch_next()
-		elseif key == 'r' then
-			score = 0
-			love.load()
-		end
+    if key == 'k' then
+        home_team:switch_next()
+    elseif key == 'r' then
+        score = 0
+        love.load()
+    end
 end
 
 function love.update(dt)
-		-- Handle Input here to ensure that inputs are handled before updating
- 		-- this makes handling collisions and movement cancellation much easier
-		-- away_team:handle_input(puck, goal)
-		home_team:handle_input(puck, goal)
-		
-		-- TODO : maybe change this method name to handle_input ? 
-		goalie:move_towards(dt, puck)
+    -- Handle Input here to ensure that inputs are handled before updating
+    -- this makes handling collisions and movement cancellation much easier
+    -- away_team:handle_input(puck, goal)
+    home_team:handle_input(puck, goal)
 
-		home_team:pickup_collision(puck)
-		away_team:pickup_collision(puck)
+    -- TODO : maybe change this method name to handle_input ?
+    goalie:move_towards(dt, puck)
 
-		if area.Collision(goalie, puck) then
-			goalie:pickup(puck)
-		end
-		
-		-- BACKEND TODO
-		-- TODO : Add enemy players
-		-- TODO : Add AI movement for team mates and enemy players
-		-- TODO : Add goal for player team
-		-- TODO : Improve goalie AI 
-		-- TODO : Implement scoring system + reset.
-		-- TODO : Implement tackling (enemy) players
-		-- 
+    home_team:pickup_collision(puck)
+    away_team:pickup_collision(puck)
 
-		-- FRONTEND TODO
-		-- TODO : Create some graphics !
-		away_team:foreach(function(i, player)
-			player:update(dt)
-			if area.Collision(player, goal) then
-				player:rollback(dt)
-			end
-			if area.Collision(player, goalie) then
-				player:rollback(dt)
-			end
-			if away_team:internal_collision(i) then
-				player:rollback(dt)
-			end
-			if away_team:external_collision(player, home_team) then
-				player:rollback(dt)
-			end
-		end)
+    if area.Collision(goalie, puck) then
+        goalie:pickup(puck)
+    end
 
-		home_team:foreach(function(i, player)
-			player:update(dt)
-			if area.Collision(player, goal) then
-				player:rollback(dt)
-			end
-			if area.Collision(player, goalie) then
-				player:rollback(dt)
-			end
-			if home_team:internal_collision(i) then
-				player:rollback(dt)
-			end
-			if home_team:external_collision(player, away_team) then
-				player:rollback(dt)
-			end
-		end)
+    -- BACKEND TODO
+    -- TODO : Add enemy players
+    -- TODO : Add AI movement for team mates and enemy players
+    -- TODO : Add goal for player team
+    -- TODO : Improve goalie AI
+    -- TODO : Implement scoring system + reset.
+    -- TODO : Implement tackling (enemy) players
+    --
+
+    -- FRONTEND TODO
+    -- TODO : Create some graphics !
+    away_team:foreach(function(i, player)
+        player:update(dt)
+        if area.Collision(player, goal) then
+            player:rollback(dt)
+        end
+        if area.Collision(player, goalie) then
+            player:rollback(dt)
+        end
+        if away_team:internal_collision(i) then
+            player:rollback(dt)
+        end
+        if away_team:external_collision(player, home_team.players) then
+            player:rollback(dt)
+        end
+    end)
+
+    home_team:foreach(function(i, player)
+        player:update(dt)
+        if area.Collision(player, goal) then
+            player:rollback(dt)
+        end
+        if area.Collision(player, goalie) then
+            player:rollback(dt)
+        end
+        if home_team:internal_collision(i) then
+            player:rollback(dt)
+        end
+        if home_team:external_collision(player, away_team.players) then
+            player:rollback(dt)
+        end
+    end)
 
     puck:update(dt)
 
-		puck:bounce(0, 0, screenWidth, screenHeight)
+    puck:bounce(0, 0, screenWidth, screenHeight)
 
-		goalie:update(dt)
+    goalie:update(dt)
 
 
-		if goal:collision(dt, puck) then
-			score = score + 1
-		end
+    if goal:collision(dt, puck) then
+        score = score + 1
+    end
 end
 
 function love.draw()
-	goal:draw()
-	away_team:draw()
-	home_team:draw()
-	puck:draw()
-	goalie:draw()
+    goal:draw()
+    away_team:draw()
+    home_team:draw()
+    puck:draw()
+    goalie:draw()
 
-	love.graphics.print("Score: " .. score, 10, screenHeight - 40)
-	love.graphics.print("Goalie" .. goalie.puck_time, 10, screenHeight - 60)
-	love.graphics.print("puck_owner: " .. puck:string(), 10, screenHeight - 80)
+    love.graphics.print("Score: " .. score, 10, screenHeight - 40)
+    love.graphics.print("Goalie" .. goalie.puck_time, 10, screenHeight - 60)
+    love.graphics.print("puck_owner: " .. puck:string(), 10, screenHeight - 80)
 end
-
--- first-lua-game/players.lua
+++ first-lua-game/players.lua
@@ -4,75 +4,75 @@ Players = {}
 Players.__index = Players
 
 function Players:new(players)
-	local obj = {
-		players = players,
-		selected = 1
-	}
-	setmetatable(obj, self)
-	return obj
+    local obj = {
+        players = players,
+        selected = 1
+    }
+    setmetatable(obj, self)
+    return obj
 end
 
 function Players:switch_next()
-	self:switch_to(self.selected + 1)
+    self:switch_to(self.selected + 1)
 end
 
 function Players:switch_to(to)
-	self.players[self.selected]:deselect()
-	self.selected = to
-	if self.selected > #self.players then
-		self.selected = 1
-	end
+    self.players[self.selected]:deselect()
+    self.selected = to
+    if self.selected > #self.players then
+        self.selected = 1
+    end
 end
 
 function Players:handle_input(puck, goal)
-		self.players[self.selected]:select()
+    self.players[self.selected]:select()
 
-		for i = 1, #self.players do
-			self.players[i]:handle_input(puck, goal)
-		end
+    for i = 1, #self.players do
+        self.players[i]:handle_input(puck, goal)
+    end
 end
 
 function Players:foreach(fn)
-	for i = 1, #self.players do 
-		fn(i, self.players[i])
-	end
+    for i = 1, #self.players do
+        fn(i, self.players[i])
+    end
 end
 
-function Players:internal_collision(i) 
-	for j = 1, #self.players do 
-		if i ~= j then
-			if area.Collision(self.players[i], self.players[j]) then
-				return true
-			end
-		end
-	end
-	return false
+function Players:internal_collision(i)
+    for j = 1, #self.players do
+        if i ~= j then
+            if area.Collision(self.players[i], self.players[j]) then
+                return true
+            end
+        end
+    end
+    return false
 end
 
-function Players:external_collision(player, players) 
-	for j = 1, #players do 
-		if area.Collision(player, players[j]) then
-			return true
-		end
-	end
-	return false
+function Players:external_collision(player, players)
+    for j = 1, #players do
+        if area.Collision(player, players[j]) then
+            return true
+        end
+    end
+    return false
 end
 
 function Players:pickup_collision(puck)
-	for i = 1, #self.players do
-		local player = self.players[i]
-		if area.Collision(player, puck) then
-			if player:pickup(puck) then
-				self:switch_to(i)
-			end
-		end
-	end
+    for i = 1, #self.players do
+        local player = self.players[i]
+        if area.Collision(player, puck) then
+            if player:pickup(puck) then
+                self:switch_to(i)
+            end
+        end
+    end
 end
 
 function Players:draw()
-	for i = 1, #self.players do
-			self.players[i]:draw()
-	end 
+    for i = 1, #self.players do
+        self.players[i]:draw()
+    end
 end
 
 return Players
