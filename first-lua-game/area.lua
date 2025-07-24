52e5395c14b1031c0bcccdad1a8d991426247f55
Lasse Martin Jakobsen <lasse@tengen.dk>
2025-07-24 15:17:50 +0000

    just saving to stay up-to-date

--- first-lua-game/area.lua
+++ first-lua-game/area.lua
@@ -1,10 +1,24 @@
+local function collision(a, b)
+    return a.x < b.x + b.width and
+        a.x + a.width > b.x and
+        a.y < b.y + b.height and
+        a.y + a.height > b.y
+end
+
 area = {
-    Collision = function(a, b)
-        return a.x < b.x + b.width and
-            a.x + a.width > b.x and
-            a.y < b.y + b.height and
-            a.y + a.height > b.y
+    CollisionResult = function(a, b)
+        local result = {
+            top = false,
+            right = false,
+            left = false,
+            bottom = false,
+        }
+        if collision(a, b) == false then
+            return result
+        end
+        -- TODO : implement collision_result here
     end,
+    Collision = collision,
     Contains = function(outer, inner)
         return inner.x >= outer.x and
             inner.y >= outer.y and
