5c9c0e681301b2c8bdfcb0df1fabdb819ed46be9
Lasse Martin Jakobsen <lasse@tengen.dk>
2025-07-26 08:24:17 +0000

    remove metatable from wall and player

--- ecs-hockey/scripts/entities/player.lua
+++ ecs-hockey/scripts/entities/player.lua
@@ -2,10 +2,9 @@ local Vector = require("scripts/types/vector")
 local Color = require("scripts/types/color")
 
 Player = {}
-Player.__index = Player
 
 function Player:new(x, y)
-    local obj = {
+    return {
         speed      = 200,
         position   = Vector:new(x, y),
         velocity   = Vector:new(0, 0),
@@ -17,8 +16,6 @@ function Player:new(x, y)
         color      = Color.RED,
         tag        = "player"
     }
-    setmetatable(obj, self)
-    return obj
 end
 
 return Player
