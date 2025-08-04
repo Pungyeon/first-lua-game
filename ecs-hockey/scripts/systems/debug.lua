7f4b33957321c0923edcd464b856602db03782db
Mobile User <mobile@ipad-4.local>
2025-08-04 14:33:34 +0200

    save local changes at 2025-08-04 14:33:34

--- ecs-hockey/scripts/systems/debug.lua
+++ ecs-hockey/scripts/systems/debug.lua
@@ -32,17 +32,18 @@ function DebugSystem:draw()
     )
 		local y = 35
 		for _, player in ipairs(self.players) do
-			local travel = v_or(player.travelling_to, Vector:new(-1, -1)):string()
-			local distance = -1
-			if travel.x != -1 then
-				player.position:distance_to(travel)
+			local travel = v_or(player.travelling_to, Vector:new(-1, -1))
+			local distance = { x = -1, y = -1, direct = -1}
+			if travel.x ~= -1 then
+				distance = player.position:distance_to(travel)
+				
 			end
 			
 			-- TODO : I need to figure out why the ai is not working. After tackling, they aren't chasing the puck AND it seems that they don't know when they've reached their target position. It's quite annoying and difficult to debug right now.
 			love.graphics.print(
 				string.format(
-					"Player: %d, Travelling: %s, Position: %s",
-					player.id, travel, player.position:string())
+					"Player: %d, Travelling: %s, Position: %s, Distance: %d",
+					player.id, travel:string(), player.position:string(), distance.direct)
 , 20, y)
 			y = y + 15
 		end
