0103387731e65eabc695a59cc461b9e2f52bebad
Mobile User <mobile@ipad-4.local>
2025-08-04 10:35:19 +0200

    save local changes at 2025-08-04 10:35:19

--- ecs-hockey/scripts/systems/debug.lua
+++ ecs-hockey/scripts/systems/debug.lua
@@ -35,7 +35,6 @@ function DebugSystem:draw()
 			local travel = v_or(player.travelling_to, Vector:new(-1, -1)):string()
 			
 			-- TODO : I need to figure out why the ai is not working. After tackling, they aren't chasing the puck AND it seems that they don't know when they've reached their target position. It's quite annoying and difficult to debug right now.
-
 			love.graphics.print(
 				string.format(
 					"Player: %d, Travelling: %s, Position: %s",
