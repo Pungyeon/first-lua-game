bb986fe1d5f980b1724ac8c4cf728f68675d4f75
Mobile User <mobile@ipad-4.local>
2025-08-09 09:07:25 +0200

    save local changes at 2025-08-09 09:07:25

--- ecs-hockey/scripts/systems/test.lua
+++ ecs-hockey/scripts/systems/test.lua
@@ -1,8 +1,14 @@
+local Vector = require("scripts/types/vector")
+local Rectangle = require("scripts/types/rectangle")
 local SystemTests = {}
 
 function SystemTests:run()
-	local start, stop = 
-  lineRectIntersect()
+  local start, stop = Vector:new(10, 10), Vector:new(100, 10)
+  local rectangle = Rectangle:new(50, 5, 20, 20)
+  local intersect = lineRectIntersect(start.x, start.y, stop.x, stop.y, rectangle)
+
+  print(string.format("Testing Line/Rectangle intersect: (expected: true, actual: %s)", intersect))
+  assert(intersect)
 end
 
 return SystemTests
