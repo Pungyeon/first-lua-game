0b34e082b54c41c2326584f940d671dfd20edb55
Mobile User <mobile@ipad-4.local>
2025-08-09 09:20:51 +0200

    save local changes at 2025-08-09 09:20:51

--- ecs-hockey/scripts/systems/test.lua
+++ ecs-hockey/scripts/systems/test.lua
@@ -2,6 +2,26 @@ local Vector = require("scripts/types/vector")
 local Rectangle = require("scripts/types/rectangle")
 local SystemTests = {}
 
+function SystemTests:setup()
+entities = {
+		Player:new(center_x + player_width * 2, center_y - player_width / 2, red_team),
+		Player:new(center_x - player_width * 3, center_y - player_width / 2, blue_team),
+		Player:new(center_x - player_width * 7, center_y - player_width / 2, blue_team),
+		Goal:new(home_goal_position, goal_width, goal_height, red_team),
+		Goal:new(away_goal_position, goal_width, goal_height, blue_team),
+		Puck:new(center_x - (puck_width / 2), center_y - (puck_height / 2), puck_width, puck_height)
+	}
+	for i = 1, #entities do
+		entities[i].id = i
+	end
+
+	local interactive_system = InteractiveSystem:new(entities)
+	local select_system = SelectSystem:new(red_team, entities)
+	EffectsSystem:init(entities)
+	AISystem:init(entities)
+
+end
+
 function SystemTests:run()
   local start, stop = Vector:new(10, 10), Vector:new(100, 10)
   local rectangle = Rectangle:new(50, 5, 20, 20)
